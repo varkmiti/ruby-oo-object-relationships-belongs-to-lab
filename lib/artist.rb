f781d272ff2603d27453bc64862588da4651f837
Mark Viti <+>
2022-06-01 11:29:29 -0400

    commit tree bbdbd3642aa710cea5a042f7bbe0e2527b1aa4a9

---
+++ lib/artist.rb
@@ -0,0 +1,7 @@
+require_relative "song.rb"
+
+class Artist
+    attr_accessor :name
+
+    Song.new
+end 
---
+++ lib/author.rb
@@ -0,0 +1,7 @@
+require_relative "post.rb"
+
+class Author 
+    attr_accessor :name
+
+    Post.new
+end 
